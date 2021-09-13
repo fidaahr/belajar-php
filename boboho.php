1be462ff5a46ffced4db5867f79299b4647283d5
fidaahr <rofidaha4@gmail.com>
2021-09-14 01:42:31 +0700

    First Commit

---
+++ boboho.php
@@ -0,0 +1,10 @@
+<?php
+    $angka = 10;
+    $sisa = $angka % 2;
+
+    if ($sisa == 1) {
+        echo $angka . " = ganjil";
+    }else{
+        echo $angka . " = genap";
+    }
+    
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+        
+        // String
+        $pembuka = "Hello World";
+        
+
+        // Integer
+        $angka = 10;
+
+
+        // float
+        $koma = 1.4;
+
+        // Boolean
+        $benar = true;
+        $salah = false;
+
+        // //////////////////////////////////////////////////////////
+
+        // Modulus
+        
+        $sisaHasilBagi = 6 % 2;
+
+
+
+
+     ?>
+     <!-- <?php echo $pembuka . '  String' ?>
+     <br>
+     <?= $angka . '  Integer'?>
+     <br>
+     <?= $koma  . '  Float' 
